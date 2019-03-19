9c600aaa55d4184cca798bbce96e843c2d6774ed
Mike Zhu <zhuyuan2706@gmail.com>
2019-03-19 21:39:31 +0800

    Add baidu analysis

---
+++ js/baidu_analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?dfeb477295d3f4dcb9a4222577e6a0dd";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
--- js/index_wave.js
+++ js/index_wave.js
@@ -479,14 +479,14 @@ class ShaderProgram {
 
   }
 
-  const pointSize = 2.5
+  const pointSize = 3.8
 
   const waves = new ShaderProgram(document.querySelector('.waves'), {
     texture: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAb1BMVEUAAAD///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////8v0wLRAAAAJHRSTlMAC/goGvDhmwcExrVjWzrm29TRqqSKenRXVklANSIUE8mRkGpv+HOfAAABCElEQVQ4y4VT13LDMAwLrUHteO+R9f/fWMfO6dLaPeKVEECRxOULWsEGpS9nULDwia2Y+ALqUNbAWeg775zv+sA4/FFRMxt8U2FZFCVWjR/YrH4/H9sarclSKdPMWKzb8VsEeHB3m0shkhVCyNzeXeAQ9Xl4opEieX2QCGnwGbj6GMyjw9t1K0fK9YZunPXeAGsfJtYjwzxaBnozGGorYz0ypK2HzQSYx1y8DgSRo2ewOiyh2QWOEk1Y9OrQV0a8TiBM1a8eMHWYnRMy7CZ4t1CmyRkhSUvP3gRXyHOCLBxNoC3IJv//ZrJ/kxxUHPUB+6jJZZHrpg6GOjnqaOmzp4NDR48OLxn/H27SRQ08S0ZJAAAAAElFTkSuQmCC',
     uniforms: {
       size: { type: 'float', value: pointSize },
       field: { type: 'vec3', value: [0, 0, 0] },
-      speed: { type: 'float', value: 6 },
+      speed: { type: 'float', value: 9.7 },
     },
     vertex: `
         #define M_PI 3.1415926535897932384626433832795
@@ -536,9 +536,9 @@ class ShaderProgram {
       const position = [], color = []
 
       const width = 400 * (w / h)
-      const depth = 400
-      const height = 3
-      const distance = 5
+      const depth = 800
+      const height = 4
+      const distance = 6
 
       for (let x = 0; x < width; x += distance) {
         for (let z = 0; z < depth; z += distance) {
