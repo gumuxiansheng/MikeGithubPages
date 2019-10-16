064f40d87172d64f307f43ce1d9343281cd90399
Mike Zhu <zhuyuan2706@gmail.com>
2019-10-16 21:40:08 +0800

    Update index

--- js/index_contents.js
+++ js/index_contents.js
@@ -2,37 +2,56 @@ const fs = require('fs');
 const path = require('path');
 const cheerio = require('cheerio');
 var readline = require('readline');
-const baseUrl = 'https://home.mikezhu.cn/docs/';
+const baseUrl = 'https://home.mikezhu.cn/';
 let list = [];
 
 fs.readdir('./docs/', function (error, files) {
     console.log(files)
+    list = [];
     files.forEach(file => {
         if(file.substring(file.length-3, file.length) === '.md'){
             list.push(file);
         }
     })
-    modifyList();
+    modifyList('docs');
 });
 
-function modifyList() {
+fs.readdir('./paper-read/', function (error, files) {
+    console.log(files)
+    list = [];
+    files.forEach(file => {
+        if(file.substring(file.length-3, file.length) === '.md'){
+            list.push(file);
+        }
+    })
+    modifyList('paper-read');
+});
+
+function modifyList(folder) {
     let content = fs.readFileSync('./index.html');
     $ = cheerio.load(content);
     let dom = $('#contents');
     dom.empty();
-    let ul = `<ol id="ol_container" class="container"></ol>`;
+    let containername = '';
+    if (folder == 'docs'){
+        containername = 'ol_container';
+    } else {
+        containername = 'ln_container';
+    }
+    let ul = `<ol id="${containername}" class="container"></ol>`;
     dom.append(ul);
-    let container = $('#ol_container');
+    let container = $(`#${containername}`);
 
 
     list.forEach((item, index) => {
-        let p = path.join('./docs/', item);
+        let p = path.join('./' + folder + '/', item);
+        console.log(p);
         let markdown = fs.readFileSync(p).toString();
 
         let markdownLines = markdown.split('\n');
         let title = markdownLines[0].replace('# ', '');
         console.log('title Out: ' + title);
-        let url = `${baseUrl}${item.substring(0, item.length - 3)}`;
+        let url = `${baseUrl}/${folder}/${item.substring(0, item.length - 3)}`;
         
         if (markdownLines.length > 2 && markdownLines[2].startsWith('<!--')){
             let timestamp = markdownLines[2].replace('<!--', '').replace('-->', '');
